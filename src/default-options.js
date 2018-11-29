2bc0c1f4050ab25491df18e8bc5049c567c6eb5f
Sebastian Maier <sebastian.maier@comci.de>
2018-11-29 10:33:08 +0100

    Added fillColor to default options

--- src/default-options.js
+++ src/default-options.js
@@ -2,11 +2,12 @@ export default {
     fontSize: 12,
     fonts: {
         'default': 'Times-Roman',
-        bold: 'Times-Bold',
-        italic: 'Times-Italic',
+        'bold': 'Times-Bold',
+        'italic': 'Times-Italic',
         'bold-italic': 'Times-BoldItalic',
         'heading-bold': 'Helvetica-Bold',
         'heading-default': 'Helvetica',
         'code': 'Courier'
-    }
+    },
+    fillColor: 'black'
 };
