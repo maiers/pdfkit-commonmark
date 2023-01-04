1ca7b3147bd91af05e5349b1bfecce8b45044742
sebastian <se@comci.de>
2023-01-04 08:45:19 +0100

    test: Added test with changed fonts

--- test/render.spec.js
+++ test/render.spec.js
@@ -4,7 +4,7 @@ import path from 'path';
 import chai, {expect} from 'chai';
 import chaiFS from 'chai-fs';
 import PDFDocument from 'pdfkit';
-import { Parser } from 'commonmark';
+import {Parser} from 'commonmark';
 import CommonmarkPDFRenderer from '../src/commonmark-pdfkit-renderer';
 import * as TestUtils from './test-utils';
 
@@ -13,7 +13,7 @@ chai.use(chaiFS);
 
 describe('final pdf render', function () {
 
-    const instance = new CommonmarkPDFRenderer({
+    let instance = new CommonmarkPDFRenderer({
         debug: true
     });
     const reader = new Parser();
@@ -25,6 +25,92 @@ describe('final pdf render', function () {
         const parsed = reader.parse(`
 
 # Some simple formats (Level 1)
+
+This is __strong__. This is _emphasized_.
+
+This is ***strong and emphasized***.
+
+## Lists (Level 2)
+
+- List a
+- List b
+- List c
+
+### Links (Level 3)
+
+This is [a link](https://www.example.com) within some text. Or another emphasized *[link](#)*. This time we 
+format [only **a part** of the string as **strong**](#). 
+
+#### Heading 4
+
+## Try CommonMark
+
+You can try CommonMark here. This dingus is powered by
+[commonmark.js](https://github.com/jgm/commonmark.js), the
+JavaScript reference implementation.
+
+# Some simple formats (Level 1)
+
+This is __strong__. This is _emphasized_.
+
+This is ***strong and emphasized***.
+
+## Lists (Level 2)
+
+- List a
+- List b
+- List c
+
+### Links (Level 3)
+
+#### Heading 4
+
+## Try CommonMark
+
+You can try CommonMark here. This dingus is powered by
+[commonmark.js](https://github.com/jgm/commonmark.js), the
+JavaScript reference implementation.
+
+
+`);
+        const writer = instance;
+
+        const doc = new PDFDocument();
+
+        doc.pipe(fs.createWriteStream(outputFilePath));
+
+        // add name of the test file
+        doc.text(`Location on disc: ${__filename}`).moveDown(2);
+
+        writer.render(doc, parsed);
+
+        doc.end();
+
+        console.log('written to', outputFilePath);
+
+        expect(outputFilePath).to.be.a.file();
+
+    });
+
+    it('use other fonts', function () {
+
+        instance = new CommonmarkPDFRenderer({
+            fonts: {
+                default: 'Helvetica',
+                bold: 'Helvetica-Bold',
+                italic: 'Helvetica-Oblique',
+                'bold-italic': 'Helvetica-BoldOblique',
+                'heading-bold': 'Courier-Bold',
+                'heading-default': 'Courier',
+                'code': 'Times',
+            },
+        });
+
+        const outputFilePath = path.join(TestUtils.outputFilePath(this));
+
+        const parsed = reader.parse(`
+
+# Header
 
 This is __strong__. This is _emphasized_.
 
@@ -343,7 +429,7 @@ lines.
             // add name of the test file
             doc.text(`Location on disc: ${outputFilePath}`).moveDown(2);
 
-            const renderedDimensions = writer.render(doc, parsed);
+            writer.render(doc, parsed);
             doc.end();
 
             console.log('written to', outputFilePath);
@@ -362,7 +448,7 @@ lines.
             // add name of the test file
             doc.text(`Location on disc: ${outputFilePath}`).moveDown(2);
 
-            const renderedDimensions = writer.render(doc, parsed, {
+            writer.render(doc, parsed, {
                 width: 200
             });
             doc.end();
